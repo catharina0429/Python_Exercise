b7598749e7483d06b8384c7692801975c1bd01d5
catharina0429 <catharina0429.js@gmail.com>
2021-10-27 00:10:56 +0200

    OOP Practice - added first class function and explanation

--- Practice/2021-10-26.py
+++ Practice/2021-10-26.py
@@ -17,11 +17,69 @@ print(f(5))   # 할당된 변수도 함수처럼 사용할 수 있음
 def my_map(func, arg_list):
     result = []
     for i in arg_list:
-        result.append(func(i)) # call square function, func == square
+        result.append(func(i)) # call(호출) square function, func == square
     return  result
 
 numeric_list = [1, 2, 3, 4, 5]
 
 square = my_map(square, numeric_list)
 print(square)
+
+# 간단한 함수 하나만을 실행할 때는 아래처럼 일반 함수를 사용할 수 있음
+# First class function을 사용하는 이유는 이미 정의된 여러 함수를 간단히 재활용 할 수 있기 때문
+def simple_square(arg_list):
+    result = []
+    for i in arg_list:
+        result.append(i * i)
+    return result
+
+simple_square_res = simple_square(numeric_list)
+print(simple_square_res)
+
+def square(x):
+    return x * x
+
+def cube(x):
+    return x * x * x
+
+def quad(x):
+    return x * x * x * x
+
+def added_my_map(func, arg_list):
+    # wrapper 한수 하나만 정의해서 기존의 함수나 모듈을 사용할 수 있음
+    result = []
+    for i in arg_list:
+        result.append(func(i))
+    return result
+
+squares_res = added_my_map(square, numeric_list)
+cubes_res = added_my_map(cube,numeric_list)
+quads_res = added_my_map(quad, numeric_list)
+
+print(squares_res)
+print(cubes_res)
+print(quads_res)
+
+# Example 2
+def logger(msg):
+    def log_message(): # closuer 클로저: 다른 함수의 지역변수를 그 함수가 종료된 이후에도 기억함
+        print('Log: ', msg)
+    return log_message
+
+log_hi = logger("Hi")
+print(log_hi) # log_message의 오브젝트가 출력됨
+log_hi() # "Log: Hi"가 출력됨
+
+del logger # global namespace에서 logger 오브젝트를 지움
+
+# logger 오브젝트가 지워진 것을 확인
+try:
+    print(logger)
+except NameError:
+    print('NameError: logger는 존재하지 않습니다.')
+
+log_hi() # logger가 지워진 뒤에도 log: Hi는 출력됨
+
+
+
 
