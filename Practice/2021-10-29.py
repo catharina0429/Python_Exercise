c487abd76c5424adb1b729f694de5978a87f4e9b
catharina0429 <catharina0429.js@gmail.com>
2021-10-29 20:26:25 +0200

    OOP Practice - added private attribute

--- Practice/2021-10-29.py
+++ Practice/2021-10-29.py
@@ -40,3 +40,15 @@ maria.put_bag('열쇠')
 
 print(james.bag)
 print(maria.bag)
+
+class Knight:
+    __item_limit = 10 # 비공개 클래스 속성
+
+    def print_item_limit(self):
+        print(Knight.__item_limit) # 클래스 안에서만 접근할 수 있음
+
+x = Knight()
+x.print_item_limit()  # 10
+
+print(Knight.__item_limit) # AttributeError 클래스 바깥에서는 접근할 수 없음
+
