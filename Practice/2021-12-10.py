6c666989698f022c88285f55b380acba1db47abb
catharina0429 <catharina0429.js@gmail.com>
2021-12-10 21:07:53 +0100

    added more python coding problem

--- Practice/2021-12-10.py
+++ Practice/2021-12-10.py
@@ -1,7 +1,6 @@
 def end_zeros(num: int) -> int:
     """Try to find out how many zeros a given number has at the end"""
     return len(str(num)) - len(str(num).rstrip('0'))
-
 if __name__ == '__main__':
     print("Example:")
     print(end_zeros(0))
@@ -18,7 +17,6 @@ if __name__ == '__main__':
 def backward_string(val:str) -> str:
     """return a given string in reverse order"""
     return val[::-1]
-
 if __name__ == '__main__':
     print("Example:")
     print(backward_string('val'))
@@ -39,7 +37,6 @@ def remove_all_before(items: list, border: int) -> Iterable:
         return items[i:]
     else:
         return items
-
 if __name__ == '__main__':
     print("Example:")
     print(list(remove_all_before([1, 1, 2, 4, 2, 3, 4], 2)))
@@ -59,7 +56,6 @@ def is_all_upper(text:str) -> bool:
         if char.islower():
             return False
     return True
-
 if __name__ == '__main__':
     print("Example:")
     print(is_all_upper('mixed UPPER and lower'))
@@ -74,3 +70,58 @@ if __name__ == '__main__':
     assert is_all_upper('55 55 5') == True
     print("Coding complete? Click 'Check' to earn cool rewards!")
 
+def replace_first(items: list) -> Iterable:
+    """In a given list the first element should become the last one"""
+    if len(items) <= 1:
+        return items
+    else:
+        tmp = items[1:]
+        tmp.append(items[0])
+        return tmp
+if __name__ == "__main__":
+    print("Example:")
+    print(list(replace_first([1, 2, 3, 4])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(replace_first([1, 2, 3, 4])) == [2, 3, 4, 1]
+    assert list(replace_first([1])) == [1]
+    assert list(replace_first([])) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+def max_digit(number: int) -> int:
+    """determine which digit in the given number is the biggest"""
+    return int(max(str(number)))
+if __name__ == '__main__':
+    print("Example:")
+    print(max_digit(0))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert max_digit(0) == 0
+    assert max_digit(52) == 5
+    assert max_digit(634) == 6
+    assert max_digit(1) == 1
+    assert max_digit(10000) == 1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+def split_pairs(a):
+    """Split the string into pairs of two characters"""
+    if len(a) %2 != 0:
+        tmp = a + str('_')
+        return [tmp[i:i+2] for i in range(0, len(tmp), 2)]
+    else:
+        return [a[i:i+2] for i in range(0, len(a), 2)]
+if __name__ == '__main__':
+    print("Example:")
+    print(list(split_pairs('abcd')))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(split_pairs('abcd')) == ['ab', 'cd']
+    assert list(split_pairs('abc')) == ['ab', 'c_']
+    assert list(split_pairs('abcdf')) == ['ab', 'cd', 'f_']
+    assert list(split_pairs('a')) == ['a_']
+    assert list(split_pairs('')) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+def beginning_zeros(number: str) -> int:
+    # your code here
+    return 0
