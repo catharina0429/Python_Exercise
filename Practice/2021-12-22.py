93dee667cf7f9c3be160c2dee54df24c8727a360
catharina0429 <catharina0429.js@gmail.com>
2021-12-22 23:22:53 +0100

    py.checkio.org All the same

--- Practice/2021-12-22.py
+++ Practice/2021-12-22.py
@@ -41,3 +41,20 @@ if __name__ == '__main__':
     assert split_list([1]) == [[1], []]
     assert split_list([]) == [[], []]
     print("Coding complete? Click 'Check' to earn cool rewards!")
+
+from typing import List, Any
+def all_the_same(elements: List[Any]) -> bool:
+    """All the same"""
+    return True if len(set(elements)) == 1 or len(elements) == 0 else False
+
+if __name__ == '__main__':
+    print("Example:")
+    print(all_the_same([1, 1, 1]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert all_the_same([1, 1, 1]) == True
+    assert all_the_same([1, 2, 1]) == False
+    assert all_the_same(['a', 'a', 'a']) == True
+    assert all_the_same([]) == True
+    assert all_the_same([1]) == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
